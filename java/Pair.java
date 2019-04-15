7f6dfe78f05fa165d4e78b8ea95c21441b6d7747
delinsun <delinsun@umail.ucsb.edu>
2019-04-15 12:21:35 -0700

    set up basic classes

---
+++ java/Pair.java
@@ -0,0 +1,17 @@
+public class Pair {
+    private int x;
+    private int y;
+
+    public Pair( int x, int y ){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
