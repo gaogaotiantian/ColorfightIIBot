03b7b92ff82ca32eb7c6f6357c100f42417e029c
delinsun <delinsun@umail.ucsb.edu>
2019-04-30 15:49:28 -0700

    fix indentation issue

--- java/src/main/java/colorfightII/Colorfight.java
+++ java/src/main/java/colorfightII/Colorfight.java
@@ -44,14 +44,14 @@ public class Colorfight {
         error = (Map<Integer, Object>) info.get("error");
         max_turn = ( (Long) ( (JSONObject)info.get("info") ).get("max_turn") ).intValue();
         round_time = ( (Long) ( (JSONObject) info.get("info") ).get("round_time") ).intValue();
-        
-	    game_map = new GameMap(
+
+        game_map = new GameMap(
                 ( (Long) ( (JSONObject)info.get("info") ).get("width") ).intValue(),
                 ( (Long) ( (JSONObject)info.get("info") ).get("height") ).intValue()
         );
         game_map._update_info( (JSONObject) info.get("game_map") );
-	
-	for ( Object o:( (Map)info.get("users") ).values() ) {
+
+        for ( Object o:( (Map)info.get("users") ).values() ) {
             JSONObject user =  (JSONObject) o;
             User temp = new User();
             temp._update_info( user );
